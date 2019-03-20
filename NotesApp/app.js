fbf2620fa7004d31959842e071b1db0fa242c0dd
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-20 14:05:24 +0300

    add command

--- NotesApp/app.js
+++ NotesApp/app.js
@@ -4,9 +4,45 @@ const yargs = require('yargs')
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function () {
-        console.log('adding a new note');
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log(argv.title, argv.body);
     }
 })
 
-console.log(yargs.argv);
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing a note');
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List a note',
+    handler: function () {
+        console.log('Listing a note');
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Reading a note',
+    handler: function () {
+        console.log('Reading a note');
+    }
+})
+
+yargs.parse()
