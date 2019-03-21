5484c3de2269e5e627d0a094a2c9fb1293d93a1d
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-21 16:31:46 +0300

    map body integration

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -2,7 +2,14 @@ const request = require('request')
 
 const url = 'https://api.darksky.net/forecast/b2e89c649de07edcbac002d14b8812fa/37.8267,-122.4233'
 
-request(url, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently);
+request({url, json: true}, (error, response) => {
+    console.log(response.body.daily);
+})
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZWxpamFoOTYwOTA0IiwiYSI6ImNqdGluZXg0ZTAwOWM0M3FqYzFpOW8yeHYifQ.iPEBKI38blYmup7NSvGjKg'
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+    const latitude = response.body.features[0].center[0]
+    const longitude = response.body.features[0].center[1]
+    console.log(latitude, longitude);
 })
