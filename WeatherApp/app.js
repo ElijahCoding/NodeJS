0025588adf37b52aa23ad6196be2f422cb6c1434
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-21 18:34:36 +0300

    chain callbacks and getting variables from terminal

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -2,13 +2,20 @@ const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+const address = process.argv[2]
 
-forecast(-73.9808, 40.7648, (error, data) => {
-    console.log('ERROR:', error)
-    console.log('DATA:', data)
-})
-
-// geocode('New York', (error, data) => {
-//     console.log('ERROR: ', error);
-//     console.log('DATA: ', data);
-// })
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            
+        })
+    })
+}
