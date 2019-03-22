40650c6833b8c976216c15ad7055921b857ccdb9
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-22 11:49:57 +0300

    public path built

--- WebServer/src/app.js
+++ WebServer/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.get('', (req, res) => {
-    res.send('hello')
-})
+app.use(express.static(publicDirectoryPath))
+
 
 app.get('/help', (req, res) => {
     res.send('help page')
