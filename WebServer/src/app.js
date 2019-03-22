2bf15365af09a79d797cbe9d511115a76b36a313
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-22 13:06:45 +0300

    some templates

--- WebServer/src/app.js
+++ WebServer/src/app.js
@@ -1,16 +1,38 @@
 const express = require('express')
 const path = require('path')
+const hbs = require('hbs')
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 app.use(express.static(publicDirectoryPath))
 
-app.get('/', (req, res) => res.render('index'))
-app.get('/about', (req, res) => res.render('about'))
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Elijah'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Elijah'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'Help Text',
+        title: 'Help',
+        name: 'Elijah'
+    })
+})
 
 app.listen(3000, () => console.log('Server is up'))
