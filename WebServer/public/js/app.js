ceff834ad16fcf12d9e4daf06be8871050f4fb5e
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-22 16:35:52 +0300

    making some templates

--- WebServer/public/js/app.js
+++ WebServer/public/js/app.js
@@ -7,3 +7,15 @@ fetch('http://localhost:3000/weather?address=boston').then(response => {
         }
     })
 })
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = 
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+
+})
