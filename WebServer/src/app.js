46cca31403fbbf409c5fb63cc1c27ef915097104
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-22 12:17:03 +0300

    new views name

--- WebServer/src/app.js
+++ WebServer/src/app.js
@@ -3,10 +3,14 @@ const path = require('path')
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
 
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
 app.use(express.static(publicDirectoryPath))
 
 app.get('/', (req, res) => res.render('index'))
+app.get('/about', (req, res) => res.render('about'))
 
 app.listen(3000, () => console.log('Server is up'))
