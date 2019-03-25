641e8f2d694831b567c338299812d18a4f265e9b
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-25 12:22:30 +0300

    built task model

--- TaskApp/src/db/mongoose.js
+++ TaskApp/src/db/mongoose.js
@@ -1,1 +1,26 @@
 const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/task-app-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+})
+
+const task = new Task({
+    description: 'Task One',
+    completed: false
+})
+
+task.save().then(response => {
+    console.log(response);
+}).catch(error => {
+    console.log(error);
+})
