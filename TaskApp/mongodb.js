bd976262ba1670c2053e9e35f1d6db0cab4f0ec1
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-25 11:16:39 +0300

    preparing to handle ObjectID

--- TaskApp/mongodb.js
+++ TaskApp/mongodb.js
@@ -1,16 +1,36 @@
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
+const ObjectID = mongodb.ObjectID
 
 const url = 'mongodb://localhost:27017'
 const database = 'task-app'
 
-MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+MongoClient.connect(url, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        console.log('Unable to connect')
+    }
 
-  const db = client.db(database);
+    console.log("Connected successfully to server");
 
-  
+    const db = client.db(database);
 
-  client.close();
+    db.collection('tasks').insertMany([
+        {
+            description: 'task one',
+            completed: true
+        },
+        {
+            description: 'task two',
+            completed: false
+        },
+        {
+            description: 'task three',
+            completed: true
+        }
+    ], (error, result) => {
+        if (error) {
+            console.log(error);
+        }
+        console.log(result.ops);
+    })
 });
