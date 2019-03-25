988401d5e06df4c820d1b0af7ec462c281fddf40
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-25 14:45:02 +0300

    user show

--- TaskApp/src/index.js
+++ TaskApp/src/index.js
@@ -22,7 +22,20 @@ app.post('/users', (req, res) => {
 
 app.get('/users', (req, res) => {
     const users = User.find({}).then(users => res.send(users))
-                      .catch(error => console.log(error))
+                      .catch(error => res.status(500).send(error))
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then(user => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch(error => {
+        res.status(500).send()
+    })
 })
 
 app.post('/tasks', (req, res) => {
