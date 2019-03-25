9fc093bcde81512ff300633eb045128a265c239c
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-25 14:23:51 +0300

    creating a user

--- TaskApp/src/index.js
+++ TaskApp/src/index.js
@@ -1,7 +1,24 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+require('./db/mongoose')
+const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.post('/users', (req, res) => {
+    const user = new User(req.body)
+
+    user.save().then(() => {
+        res.status(201).send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
