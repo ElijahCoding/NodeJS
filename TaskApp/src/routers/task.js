5e744483e6120ba2d363e33557b43e1a02161ca2
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-26 15:44:53 +0300

    filter data

--- TaskApp/src/routers/task.js
+++ TaskApp/src/routers/task.js
@@ -19,9 +19,17 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+
     try {
-        // const tasks = await Task.find({ owner: req.user._id })
-        await req.user.populate('tasks').execPopulate()
+        await req.user.populate({
+            path: 'tasks',
+            match
+        }).execPopulate()
         res.status(201).send(req.user.tasks)
     } catch (e) {
         res.status(400).send(e)
