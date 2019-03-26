306ec2c7fc69c64e0486e2da5c5b04756da7f923
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-26 16:38:50 +0300

    showing avatar

--- TaskApp/src/routers/user.js
+++ TaskApp/src/routers/user.js
@@ -107,7 +107,7 @@ const upload = multer({
     }
 })
 
-router.post('/users/me/avatar', auth, upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
     req.user.avatar = req.file.buffer
     await req.user.save()
 
@@ -119,8 +119,22 @@ router.post('/users/me/avatar', auth, upload.single('avatar'), (req, res) => {
 router.delete('/users/me/avatar', auth, async (req, res) => {
     req.user.avatar = undefined
     await req.user.save()
-
     res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar)
+    } catch (e) {
+        res.status(404).send()
+    }
 })
 
 module.exports = router
