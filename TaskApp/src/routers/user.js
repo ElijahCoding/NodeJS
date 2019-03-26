ffd2062a654c119b68458fcf6550ecbe96a265da
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-26 16:48:56 +0300

    optimize the photo

--- TaskApp/src/routers/user.js
+++ TaskApp/src/routers/user.js
@@ -3,6 +3,7 @@ const router = new express.Router()
 const auth = require('../middleware/auth')
 const User = require('../models/user')
 const multer = require('multer')
+const sharp = require('sharp')
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -108,7 +109,9 @@ const upload = multer({
 })
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+    req.user.avatar = buffer
+
     await req.user.save()
 
     res.send()
