c7b86fb8aab273922d8a6366beaba4e065f5d27b
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-26 17:05:49 +0300

    sending email when user created

--- TaskApp/src/emails/account.js
+++ TaskApp/src/emails/account.js
@@ -1,1 +1,28 @@
+const sgMail = require('@sendgrid/mail')
+
 const sendgridAPIKey = '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'
+
+sgMail.setApiKey(sendgridAPIKey)
+
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'cuiyeqing960904@gmail.com',
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    })
+}
+
+const sendCancelationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'cuiyeqing960904@gmail.com',
+        subject: 'Sorry to see you go!',
+        text: `Goodbye, ${name}. I hope to see you back sometime soon.`
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail
+}
