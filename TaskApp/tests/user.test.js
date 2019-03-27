f62e4fcd02b1575ca22e177dff689acefcecb486
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-27 13:41:34 +0300

    test signup endpoint

--- TaskApp/tests/user.test.js
+++ TaskApp/tests/user.test.js
@@ -1,1 +1,22 @@
 const request = require('supertest')
+const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+    name: 'Mike',
+    email: 'mike@example.com',
+    password: '56what!!'
+}
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
+
+test('Should signup a new user', async () => {
+    await request(app).post('/users').send({
+        name: 'Elijah',
+        email: 'elijah@gmail.com',
+        password: 'hellojava'
+    }).expect(201)
+})
