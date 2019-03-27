065a0a271382ef5e1d945c95a25b8ab27bba8ff7
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-27 15:24:45 +0300

    delete user endpoints

--- TaskApp/tests/user.test.js
+++ TaskApp/tests/user.test.js
@@ -42,3 +42,29 @@ test('Should ot login nonexistent user', async () => {
         password: 'fd'
     })
 })
+
+test('Should get profile for user', async () => {
+    await request(app).get('/users/me')
+                      .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+                      .send()
+                      .expect(200)
+})
+
+test('Should not get profile for unauthenticated user', async () => {
+    await request(app).get('/users/me')
+                      .send()
+                      .expect(401)
+
+})
+
+test('Should delete account for user', async () => {
+    await request(app).delete('/users/me')
+                      .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+                      .send()
+})
+
+test('Should not delete account for unauthenticated user', async () => {
+    await request(app).delete('/users/me')
+                      .send()
+                      .expect(401)
+})
