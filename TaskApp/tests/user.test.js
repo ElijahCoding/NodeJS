ae9437877148a28e71c431cbefc24bc27d1162f8
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-27 16:08:24 +0300

    update endpoint testing

--- TaskApp/tests/user.test.js
+++ TaskApp/tests/user.test.js
@@ -87,3 +87,32 @@ test('Should not delete account for unauthenticated user', async () => {
                       .send()
                       .expect(401)
 })
+
+test('Should upload avatar image', async () => {
+    await request(app).post('/users/me/avatar')
+                      .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+                      .attach('avatar', 'tests/fixures/doc.png')
+                      .expect(200)
+    const user = await User.findById(userOneId)
+    expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async () => {
+    await request(app).patch('/users/me')
+                      .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+                      .send({
+                          name: 'ivan'
+                      })
+                      .expect(200)
+    const user = await User.findById(userOneId)
+    expect(user.name).toEqual('ivan')
+})
+
+test('Should not update invalid user fields', async () => {
+    await request(app).patch('/users/me')
+                      .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+                      .send({
+                          localtion: 'russia'
+                      })
+                      .expect(400)
+})
