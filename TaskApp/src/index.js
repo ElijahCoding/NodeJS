cf66ede8e63ed5468650d1865d41d956a6b73a84
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-27 13:34:56 +0300

    restructured app.js

--- TaskApp/src/index.js
+++ TaskApp/src/index.js
@@ -1,18 +1,7 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
+const app = require('./app.js')
 
-const app = express()
 const port = process.env.PORT
 
-app.use(
-    express.json(), bodyParser.json(),
-    userRouter, taskRouter, bodyParser.urlencoded({ extended: false })
-)
-
-
 app.listen(port, () => {
-    console.log(`Server is up on port ${port}`)
+    console.log('Server is up on port ' + port);
 })
