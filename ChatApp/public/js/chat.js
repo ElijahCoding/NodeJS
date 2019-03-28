a12a9a6a2bee361d8c87a1dc9557ac903712d201
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-28 11:16:10 +0300

    sendMessage socket

--- ChatApp/public/js/chat.js
+++ ChatApp/public/js/chat.js
@@ -1,9 +1,13 @@
 const socket = io()
 
-socket.on('countUpdated', (count) => {
-    console.log(count);
+socket.on('message', (message) => {
+    console.log(message);
 })
 
-document.querySelector('#increment').addEventListener('click', () => {
-    socket.emit('increment')
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const message = document.querySelector('input').value
+
+    socket.emit('sendMessage', message)
 })
--- ChatApp/src/index.js
+++ ChatApp/src/index.js
@@ -13,14 +13,11 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
-
 io.on('connection', (socket) => {
-    console.log('New WebSocket Connection')
+    socket.emit('message', 'Welcome')
 
-    socket.on('increment', () => {
-        count++
-        io.emit('countUpdated', count)
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
     })
 })
 
