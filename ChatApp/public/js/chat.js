319dd6a1f93318ab1507eefdff02d3403e13255c
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-29 10:52:10 +0300

    renering location message

--- ChatApp/public/js/chat.js
+++ ChatApp/public/js/chat.js
@@ -7,10 +7,18 @@ const $sendLocationButton = document.querySelector('#send-location')
 const $messages = document.querySelector('#messages')
 
 const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
 
 socket.on('message', (message) => {
     const html = Mustache.render(messageTemplate, {
         message
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('locationMessage', (url) => {
+    const html = Mustache.render(locationMessageTemplate, {
+        url
     })
     $messages.insertAdjacentHTML('beforeend', html)
 })
