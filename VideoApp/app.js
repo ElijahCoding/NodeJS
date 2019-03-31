6b05dbcc4d92def187e0e42279472b097490b4e5
ElijahCoding <cuiyeqing960904@gmail.com>
2019-03-31 15:50:32 +0300

    handlebar built

--- VideoApp/app.js
+++ VideoApp/app.js
@@ -1,7 +1,16 @@
 const express = require('express')
+var exphbs  = require('express-handlebars')
+
 const app = express()
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
 const port = 5000
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
 
 app.listen(port, () => {
     console.log(`Server runs on port ${port}`)
