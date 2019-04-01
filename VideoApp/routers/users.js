b3809d79953a199eb90b9bf690d1a66bfe128474
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-01 22:10:22 +0300

    auth validation

--- VideoApp/routers/users.js
+++ VideoApp/routers/users.js
@@ -9,4 +9,16 @@ router.get('/register', (req, res) => {
     res.render('users/register')
 })
 
+router.post('/register', (req, res) => {
+    let errors = []
+
+    if (req.body.password != req.body.password2) {
+        errors.push({ text: 'Passwords do not match' })
+    }
+
+    if (req.body.password.length <= 4) {
+        errors.push({ text:'Password must be at least 4 characters' });
+    }
+})
+
 module.exports = router
