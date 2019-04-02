89ffc3ad6656859a0e78da5b76d9c39704c50f46
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-02 15:05:37 +0300

    access control

--- VideoApp/routers/ideas.js
+++ VideoApp/routers/ideas.js
@@ -7,7 +7,7 @@ require('../models/Idea')
 const Idea = mongoose.model('ideas')
 
 router.get('/', ensureAuthenticated, (req, res) => {
-    Idea.find({})
+    Idea.find({ user: req.user.id })
         .sort({ date: 'desc' })
         .then(ideas => {
             res.render('ideas/index', {
@@ -24,9 +24,14 @@ router.get('/edit/:id', ensureAuthenticated, (req, res) => {
     Idea.findOne({
         _id: req.params.id
     }).then(idea => {
-        res.render('ideas/edit' ,{
-            idea
-        })
+        if (idea.user =! req.user.id) {
+            req.flash('error_msg', 'Not Authorized')
+            res.redirect('/ideas')
+        } else {
+            res.render('ideas/edit' ,{
+                idea
+            })
+        }
     })
 })
 
@@ -50,7 +55,8 @@ router.post('/', (req, res) => {
     } else {
         const newUser = {
             title: req.body.title,
-            details: req.body.details
+            details: req.body.details,
+            user: req.user.id
         }
 
         new Idea(newUser).save()
