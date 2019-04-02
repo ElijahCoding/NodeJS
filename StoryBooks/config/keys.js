662bc1f3b60ee2195900af9a603de88d04066400
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-02 17:32:55 +0300

    configuration set up

---
+++ StoryBooks/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://root:test1234$@ds229826.mlab.com:29826/storybooks',
+    googleClientID: '466751898413-6fgv7h2r7ca6kibk8eh4r9532qs1f4l5.apps.googleusercontent.com',
+    googleClientSecret: 'mLSF1WguusOEBvwhC9cGST55'
+}
