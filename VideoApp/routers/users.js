15110f56e19823da0ddf553c2f954a7b80c2f395
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-02 14:53:31 +0300

    logout funciton

--- VideoApp/routers/users.js
+++ VideoApp/routers/users.js
@@ -73,11 +73,13 @@ router.post('/register', (req, res) => {
                     })
                 }
             })
-
-
     }
+})
 
-
+router.get('/logout', (req, res) => {
+    req.logout()
+    req.flash('success_msg', 'You are logged out')
+    res.redirect('/users/login')
 })
 
 module.exports = router
