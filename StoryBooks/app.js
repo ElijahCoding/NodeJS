213b7ed2de3dd4b9ed218cba744b56836f4c88e2
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-02 18:58:24 +0300

    user model

--- StoryBooks/app.js
+++ StoryBooks/app.js
@@ -2,8 +2,16 @@ const express = require('express')
 const mongoose = require('mongoose')
 const passport = require('passport')
 const auth = require('./routes/auth')
+const keys = require('./config/keys')
 
 require('./config/passport')(passport)
+
+mongoose.Promise = global.Promise
+
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true
+}).then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err))
 
 const app = express()
 
