3dad0a9fcc3bb955d1ff4df488c9b00a77d646b9
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-02 14:38:03 +0300

    passport strategy finished

--- VideoApp/app.js
+++ VideoApp/app.js
@@ -37,12 +37,15 @@ app.use(session({
 }))
 app.use(flash())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Global variables
 app.use(function (req, res, next) {
     res.locals.success_msg = req.flash('success_msg')
     res.locals.error_msg = req.flash('error_msg')
     res.locals.error = req.flash('error')
+    res.locals.user = req.user || null
     next()
 })
 
