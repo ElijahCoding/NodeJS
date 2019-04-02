8046b5e161a07dbfd3fa02b05f40e25bed99db2c
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-02 19:29:03 +0300

    secret built

--- VideoApp/config/database.js
+++ VideoApp/config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
     module.exports = {
-        mongoURI: 'mongodb://root:Vladimir960904@ds229826.mlab.com:29826/video-app'
+        mongoURI: 'mongodb://root:test1234@ds229826.mlab.com:29826/video-app'
     }
 } else {
     module.exports = {
