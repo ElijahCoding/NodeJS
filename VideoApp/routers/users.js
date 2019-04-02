b55d5f4e0dd781ae4c5f98ba17289f120a82dc5a
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-02 14:06:23 +0300

    register a user

--- VideoApp/routers/users.js
+++ VideoApp/routers/users.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const router = new express.Router()
+const bcrypt = require('bcryptjs')
+const passport = require('passport')
+const mongoose = require('mongoose')
+
+require('../models/User')
+const User = mongoose.model('users')
 
 router.get('/login', (req, res) => {
     res.send('login')
@@ -28,7 +34,42 @@ router.post('/register', (req, res) => {
             password: req.body.password,
             password2: req.body.password2
         })
+    } else {
+        User.findOne({ email: req.body.email })
+            .then(user => {
+                if (user) {
+                    req.flash('error_msg', 'Email already registered')
+                    res.redirect('/users/login')
+                } else {
+                    const newUser = new User({
+                        name: req.body.name,
+                        email: req.body.email,
+                        password: req.body.password,
+                    })
+
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if (err) throw err
+                            newUser.password = hash
+
+                            newUser.save()
+                                   .then(user => {
+                                       req.flash('success_msg', 'You are registered')
+                                       res.redirect('/users/login')
+                                   })
+                                   .catch(err => {
+                                       console.log(err)
+                                       return
+                                   })
+                        })
+                    })
+                }
+            })
+
+
     }
+
+
 })
 
 module.exports = router
