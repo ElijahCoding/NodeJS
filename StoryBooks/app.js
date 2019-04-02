538db91e8f82741aa2870aa53ebb00090b63e737
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-02 20:29:31 +0300

    body parser

--- StoryBooks/app.js
+++ StoryBooks/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const passport = require('passport')
 const exphbs = require('express-handlebars')
@@ -22,6 +23,9 @@ mongoose.connect(keys.mongoURI, {
   .catch(err => console.log(err))
 
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.set('view engine', 'handlebars')
 app.engine('handlebars', exphbs({
