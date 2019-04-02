22d690a2284d7ff22d63e63085abcba178726e9e
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-02 19:27:52 +0300

    auth in place gaga

--- StoryBooks/routes/auth.js
+++ StoryBooks/routes/auth.js
@@ -15,4 +15,17 @@ router.get(
     }
 )
 
+router.get('/verify', (req, res) => {
+    if (req.user) {
+        console.log(req.user);
+    } else {
+        console.log('NOT AUTH');
+    }
+})
+
+router.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
+
 module.exports = router
