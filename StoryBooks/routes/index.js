be0922caef11e30111a8b863b394d2780d3a4e2d
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-03 09:13:58 +0300

    dashboard template built

--- StoryBooks/routes/index.js
+++ StoryBooks/routes/index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+const Story = mongoose.model('stories')
+const { ensureGuest, ensureAuthenticated } = require('../helpers/auth')
 
 router.get('/', (req, res) => {
     res.render('index/welcome')
 })
 
-router.get('/dashboard', (req, res) => {
-    res.render('index/dashboard')
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
+    Story.find({ user: req.user.id }).then(stories => {
+        res.render('index/dashboard', {
+            stories
+        })
+    })
 })
 
 router.get('/about', (req, res) => {
