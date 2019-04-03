f6f1aa78b6b66951cf218f51305d9e2d6bfc00cb
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-03 11:28:29 +0300

    stories by user and my own stories

--- StoryBooks/routes/stories.js
+++ StoryBooks/routes/stories.js
@@ -31,6 +31,26 @@ router.get('/show/:id', (req, res) => {
          })
 })
 
+router.get('/user/:userId', (req, res) => {
+    Story.find({ user: req.params.userId, status: 'public' })
+         .populate('user')
+         .then(stories => {
+             res.render('stories/index', {
+                 stories
+             })
+         })
+})
+
+router.get('/my', (req, res) => {
+    Story.find({ user: req.user.id })
+         .populate('user')
+         .then(stories => {
+             res.render('stories/index', {
+                 stories
+             })
+         })
+})
+
 router.get('/add', ensureAuthenticated, (req, res) => {
   res.render('stories/add')
 })
