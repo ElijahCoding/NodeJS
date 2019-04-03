b6e35acaa37d3ab0e7d18b8e8b6db294056b0c3d
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-03 12:32:31 +0300

    deloyment settings

--- StoryBooks/config/keys.js
+++ StoryBooks/config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://root:test1234$@ds229826.mlab.com:29826/storybooks',
-    googleClientID: '466751898413-6fgv7h2r7ca6kibk8eh4r9532qs1f4l5.apps.googleusercontent.com',
-    googleClientSecret: 'mLSF1WguusOEBvwhC9cGST55'
+if(process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod')
+} else {
+  module.exports = require('./keys_dev')
 }
