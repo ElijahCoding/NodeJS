35dfd52e047ca7d12f9c1feb1d541367434f54bf
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-11 19:52:06 +0300

    jade template built

--- BallControl/server.js
+++ BallControl/server.js
@@ -1,9 +1,20 @@
 const app = require('express')()
-const server = require('http').Server()
 const jade = require('jade')
 
-const port = 3000
+const port = 3001
 
-server.listen(port, () => {
+app.set('views', __dirname + '/templates')
+app.set('view engine', 'jade')
+
+app.get('/', (req, res, next) => {
+    res.render('game', {
+        id: 'abc',
+        title: 'hi',
+        host: req.headers.host
+    })
+
+})
+
+app.listen(port, () => {
     console.log('Server working')
 })
