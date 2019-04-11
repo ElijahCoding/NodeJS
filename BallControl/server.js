3045a9338bec41a8164e98db75a71dc80984bb44
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-11 20:26:41 +0300

    building socket connection

--- BallControl/server.js
+++ BallControl/server.js
@@ -10,6 +10,20 @@ app.set('view engine', 'jade')
 
 app.get('/', (req, res, next) => {
     var id = require('./helpers/randomstring')(3)
+    var room = io.of('/' + id)
+
+    room.on('connection', (socket) => {
+        var ready = false
+
+        socket.on('movement', function (data) {
+            if (!ready) {
+                room.emit('connected')
+                ready = true
+            }
+            room.emit('updateBall', data)
+        })
+    })
+
     res.render('game', {
         id,
         title: 'Game',
