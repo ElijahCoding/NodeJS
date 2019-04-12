0eb75f3ed67daecac01e35afc2bd8fdeaf85a241
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-12 14:04:22 +0300

    ball control finished

--- BallControl/server.js
+++ BallControl/server.js
@@ -1,44 +1,52 @@
-const app = require('express')()
-const jade = require('jade')
-const http = require('http')
-const server = http.createServer(app)
-const io = require('socket.io')(server)
-const port = 3001
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var jade = require('jade');
 
-app.set('views', __dirname + '/templates')
-app.set('view engine', 'jade')
+var port = 3000;
 
-app.get('/', (req, res, next) => {
-    var id = require('./helpers/randomstring')(3)
-    var room = io.of('/' + id)
+server.listen(port, function () {
+    console.log('Server started.');
+});
 
-    room.on('connection', (socket) => {
-        var ready = false
+app.get('/', function (req, res, next) {
+    var id = require('./helpers/randomstring')(3);
+    var room = io.of('/' + id);
+
+    room.on('connection', function (socket) {
+        var ready = false;
 
         socket.on('movement', function (data) {
             if (!ready) {
-                room.emit('connected')
-                ready = true
+                room.emit('connected');
+                ready = true;
             }
-            room.emit('updateBall', data)
-        })
-    })
 
-    res.render('game', {
-        id,
-        title: 'Game',
-        host: req.headers.host
-    })
-})
+            room.emit('updateBall', data);
+        });
+    });
 
-app.get('/:id', (req, res, next) => {
-    res.render('controller', {
-        id: req.params.id,
-        title: 'Controller',
-        host: req.headers.host
-    })
-})
+    try {
+        res.send(jade.compileFile(__dirname + '/templates/game.jade')({
+            id: id,
+            title: 'Game',
+            host: req.hostname,
+            port: port
+        }));
+    } catch (e) {
+        next(e)
+    }
+});
 
-server.listen(port, () => {
-    console.log('Server working')
-})
+app.get('/:id', function (req, res, next) {
+    try {
+        res.send(jade.compileFile(__dirname + '/templates/controller.jade')({
+            id: req.params.id,
+            title: 'Controller',
+            host: req.hostname,
+            port: port
+        }));
+    } catch (e) {
+        next(e)
+    }
+});
