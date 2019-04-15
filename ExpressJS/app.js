fbc47785db12b5729dfe9cf897d7db0e3e63e5cc
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-15 11:01:55 +0300

    bug fixed

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
-const ejs = require('ejs')
 
 const app = express()
 
@@ -22,7 +21,6 @@ app.use((req, res, next) => {
   res.status(404).render('404', { pageTitle: 'Page Not Found' })
 })
 
-
-server.listen(3000, () => {
+app.listen(3000, () => {
     console.log('Listening')
 })
