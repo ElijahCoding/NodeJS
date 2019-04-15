29bed101e53572822aed3dff5cf9ced3d8182b39
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-15 11:19:52 +0300

    building controllers

--- ExpressJS/routes/admin.js
+++ ExpressJS/routes/admin.js
@@ -1,23 +1,12 @@
-const path = require('path');
-
-const express = require('express');
-
-const rootDir = require('../util/path');
-
-const router = express.Router();
+const path = require('path')
+const express = require('express')
+const router = express.Router()
+const productsController = require('../controllers/products')
 
 const products = [];
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-});
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
