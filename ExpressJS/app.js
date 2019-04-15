04b54a82a0d091b5f1e08ad4bba9400b34a2a9d3
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-15 11:00:13 +0300

    mvc structure built

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -1,9 +1,27 @@
-const http = require('http')
+const path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
 
 const app = express()
 
-const server = http.createServer(app)
+app.set('view engine', 'ejs')
+app.set('views', 'view')
+
+const adminData = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' })
+})
+
 
 server.listen(3000, () => {
     console.log('Listening')
