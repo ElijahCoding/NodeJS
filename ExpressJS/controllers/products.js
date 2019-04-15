2865aa4fbe86f2ff38f4f2660dcc38b97a8e5b57
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-15 12:53:50 +0300

    refactoring to controllers

--- ExpressJS/controllers/products.js
+++ ExpressJS/controllers/products.js
@@ -7,3 +7,20 @@ exports.getAddProduct = (req, res, next) => {
     activeAddProduct: true
   });
 }
+
+exports.postAddProduct = (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+}
+
+exports.getProducts = (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
+}
--- ExpressJS/routes/admin.js
+++ ExpressJS/routes/admin.js
@@ -5,14 +5,9 @@ const productsController = require('../controllers/products')
 
 const products = [];
 
-// /admin/add-product => GET
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', productsController.getAddProduct)
 
-// /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+router.post('/add-product', productsController.postAddProduct)
 
 exports.routes = router;
 exports.products = products;
