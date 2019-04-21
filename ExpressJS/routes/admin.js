e902d7277b9d03d17023827b9ef1a3091c17cf28
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-21 17:33:24 +0300

    routes-controllers built

--- ExpressJS/routes/admin.js
+++ ExpressJS/routes/admin.js
@@ -1,13 +1,16 @@
 const path = require('path')
 const express = require('express')
 const router = express.Router()
-const productsController = require('../controllers/products')
+
+const adminController = require('../controllers/admin')
 
 const products = [];
 
-router.get('/add-product', productsController.getAddProduct)
+router.get('/add-product', adminController.getAddProduct);
 
-router.post('/add-product', productsController.postAddProduct)
+router.get('/products', adminController.getProducts);
+
+router.post('/add-product', adminController.postAddProduct);
 
 exports.routes = router;
 exports.products = products;
