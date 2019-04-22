53f6b768fa18617d48398c2c9f85dbf892ab4e8e
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-22 18:00:08 +0300

    jwt built

--- RestAPI/controllers/auth.js
+++ RestAPI/controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator/check')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/User')
 
@@ -56,5 +57,14 @@ exports.login = (req, res, next) => {
                 error.statusCode = 401
                 throw error
             }
+            const token = jwt.sign(
+                {
+                  email: loadedUser.email,
+                  userId: loadedUser._id.toString()
+                },
+                'somesupersecretsecret',
+                { expiresIn: '1h' }
+            )
+            res.status(200).json({ token, userId: loadedUser._id.toString() })
         })
 }
