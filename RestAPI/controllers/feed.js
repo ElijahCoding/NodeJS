6e2757ce1d732182bb37dcf12c06adb7a84c7c88
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-22 15:47:06 +0300

    save post

--- RestAPI/controllers/feed.js
+++ RestAPI/controllers/feed.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator/check')
+const Post = require('../models/post')
 
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
@@ -15,15 +16,27 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-        const error = new Error('Validation failed, entered data is incorrect.');
-        error.statusCode = 422;
-        throw error;
+      return res.status(422).json({
+        message: 'Validation failed, entered data is incorrect.',
+        errors: errors.array()
+      })
     }
+
     const title = req.body.title;
     const content = req.body.content;
-    // Create post in db
-    res.status(201).json({
-      message: 'Post created successfully!',
-      post: { id: new Date().toISOString(), title: title, content: content }
-    });
+    const post = new Post({
+      title: title,
+      content: content,
+      imageUrl: 'images/duck.jpg',
+      creator: { name: 'elijah' }
+    })
+
+    post.save().then(post => {
+        res.status(201).json({
+          message: 'Post created successfully!',
+          post
+        });
+    }).catch(err => {
+        console.log(err)
+    })
 };
