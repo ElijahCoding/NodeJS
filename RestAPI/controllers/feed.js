f2799737fc53a39b942414b6f8e3371c8a8ac24a
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-22 15:50:49 +0300

    validation-errors and static path

--- RestAPI/controllers/feed.js
+++ RestAPI/controllers/feed.js
@@ -14,29 +14,32 @@ exports.getPosts = (req, res, next) => {
 }
 
 exports.createPost = (req, res, next) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      return res.status(422).json({
-        message: 'Validation failed, entered data is incorrect.',
-        errors: errors.array()
-      })
-    }
-
-    const title = req.body.title;
-    const content = req.body.content;
-    const post = new Post({
-      title: title,
-      content: content,
-      imageUrl: 'images/duck.jpg',
-      creator: { name: 'elijah' }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error;
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'images/duck.jpg',
+    creator: { name: 'Elijah' }
+  });
+  post
+    .save()
+    .then(result => {
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post: result
+      });
     })
-
-    post.save().then(post => {
-        res.status(201).json({
-          message: 'Post created successfully!',
-          post
-        });
-    }).catch(err => {
-        console.log(err)
-    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
