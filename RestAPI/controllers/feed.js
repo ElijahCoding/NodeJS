89d0b0dab2edb23c467115fbf333ccc9a2fa2339
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-22 15:03:05 +0300

    validation

--- RestAPI/controllers/feed.js
+++ RestAPI/controllers/feed.js
@@ -1,15 +1,29 @@
+const { validationResult } = require('express-validator/check')
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-        posts: [{ title: 'First Post', content: 'This is the first post!' }]
+        posts: [
+            {
+                title: 'First Post',
+                content: 'This is the first post!',
+                imageUrl: 'images/duck.jpg',
+            }
+        ]
     })
 }
 
 exports.createPost = (req, res, next) => {
-  const title = req.body.title;
-  const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
-  });
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed, entered data is incorrect.');
+        error.statusCode = 422;
+        throw error;
+    }
+    const title = req.body.title;
+    const content = req.body.content;
+    // Create post in db
+    res.status(201).json({
+      message: 'Post created successfully!',
+      post: { id: new Date().toISOString(), title: title, content: content }
+    });
 };
