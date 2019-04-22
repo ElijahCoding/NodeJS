ffa359078e19106f925a8c2a55f31e84d0a3f5b1
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-22 09:40:30 +0300

    cart - product built

--- ExpressJS/routes/shop.js
+++ ExpressJS/routes/shop.js
@@ -1,19 +1,25 @@
-const path = require('path')
-const express = require('express')
-const router = express.Router()
+const path = require('path');
 
-const shopController = require('../controllers/shop')
+const express = require('express');
 
-router.get('/', shopController.getIndex)
+const shopController = require('../controllers/shop');
 
-router.get('/products', shopController.getProducts)
+const router = express.Router();
 
-router.get('/products/:productId', shopController.getProduct)
+router.get('/', shopController.getIndex);
 
-router.get('/cart', shopController.getCart)
+router.get('/products', shopController.getProducts);
 
-router.get('/checkout', shopController.getCheckout)
+router.get('/products/:productId', shopController.getProduct);
 
-router.get('/orders', shopController.getOrders)
+router.get('/cart', shopController.getCart);
+
+router.post('/cart', shopController.postCart);
+
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
