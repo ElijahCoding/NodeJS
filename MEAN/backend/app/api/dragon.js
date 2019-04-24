b8e8c0486c812f5a9081a7c1719b4259423a1b39
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-25 00:12:59 +0300

    routes built

--- MEAN/backend/app/api/dragon.js
+++ MEAN/backend/app/api/dragon.js
@@ -2,7 +2,7 @@ const { Router } = require('express')
 const router = new Router()
 
 router.get('/new', (req, res, next) => {
-    res.json({ dragon: engine.generation.newDragon() })
+    res.json({ dragon: req.app.locals.engine.generation.newDragon() })
 })
 
 module.exports = router
--- MEAN/backend/app/index.js
+++ MEAN/backend/app/index.js
@@ -2,12 +2,16 @@ const GenerationEngine = require('./generation/engine')
 const express = require('express')
 
 const dragonRouter = require('./api/dragon')
+const generationRouter = require('./api/generation')
 
 const app = express()
 const engine = new GenerationEngine()
 
+app.locals.engine = engine
+
 engine.start()
 
 app.use('/dragon', dragonRouter)
+app.use('/generation', generationRouter)
 
 module.exports = app
