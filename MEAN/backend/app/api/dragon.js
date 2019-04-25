95425f382ac1e0dfa509aba2eeedcf790cb1ece1
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-25 11:49:20 +0300

    bad practice

--- MEAN/backend/app/api/dragon.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express')
-const router = new Router()
-
-router.get('/new', (req, res, next) => {
-    res.json({ dragon: req.app.locals.engine.generation.newDragon() })
-})
-
-module.exports = router
--- MEAN/backend/app/api/generation.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express')
-const router = new Router()
-
-router.get('/', (req, res, next) => {
-    res.json({ generation: req.app.locals.engine.generation })
-})
-
-module.exports = router
