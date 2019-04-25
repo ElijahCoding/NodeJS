6a85c0b5b43aad4dc62b2ae41820685e82374709
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-25 13:59:06 +0300

    google auth strategy built

---
+++ MEAN/server/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientID: '685511722882-tadvrlremlm3ehopudngt3s2tefbkpug.apps.googleusercontent.com',
+    googleClientSecret: '-qO8-vGl8nP8Tlbgfa1uvF4f'
+}
--- MEAN/server/index.js
+++ MEAN/server/index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const app = express()
+const keys = require('./config/keys')
 
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 
-// passport.use(new GoogleStrategy({
-//
-// }))
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken) => {
+        console.log(accessToken);
+    })
+)
+
+app.get('/auth/google')
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT)
