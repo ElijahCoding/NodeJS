5b3925d84b3638e925a8f5fe3784080f015aae92
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-26 14:54:54 +0300

    building models

--- MEAN/server/config/keys.js
+++ MEAN/server/config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     googleClientID: '685511722882-tadvrlremlm3ehopudngt3s2tefbkpug.apps.googleusercontent.com',
-    googleClientSecret: '-qO8-vGl8nP8Tlbgfa1uvF4f'
+    googleClientSecret: '-qO8-vGl8nP8Tlbgfa1uvF4f',
+    mongoURI: 'mongodb://root:hellojava4@ds137596.mlab.com:37596/ecommerce'
 }
