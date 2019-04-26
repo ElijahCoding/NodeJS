328b8475c770d852789a6f5ca15885aa6913023c
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-26 14:38:59 +0300

    routes built

--- MEAN/server/index.js
+++ MEAN/server/index.js
@@ -1,25 +1,9 @@
 const express = require('express')
+require('./services/passport')
+
 const app = express()
-const keys = require('./config/keys')
 
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-
-passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, (accessToken) => {
-        console.log(accessToken);
-    })
-)
-
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}))
-
-app.get('/auth/google/callback', passport.authenticate('google'))
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT)
