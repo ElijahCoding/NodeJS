07ac6cd6dd439bc3f8583a7cf3e089db2c24767e
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-26 18:55:19 +0300

    dev env built and prepared client-side

---
+++ MEAN/server/config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '685511722882-tadvrlremlm3ehopudngt3s2tefbkpug.apps.googleusercontent.com',
+    googleClientSecret: '-qO8-vGl8nP8Tlbgfa1uvF4f',
+    mongoURI: 'mongodb://root:hellojava4@ds137596.mlab.com:37596/ecommerce',
+    cookieKey: 'secret'
+}
--- MEAN/server/config/keys.js
+++ MEAN/server/config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-    googleClientID: '685511722882-tadvrlremlm3ehopudngt3s2tefbkpug.apps.googleusercontent.com',
-    googleClientSecret: '-qO8-vGl8nP8Tlbgfa1uvF4f',
-    mongoURI: 'mongodb://root:hellojava4@ds137596.mlab.com:37596/ecommerce',
-    cookieKey: 'secret'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod')
+} else {
+    module.exports = require('./dev')
 }
