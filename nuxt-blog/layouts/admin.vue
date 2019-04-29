c5c7d70da8a205295438d31ee8ce492d787ca19f
ElijahCoding <cuiyeqing960904@gmail.com>
2019-04-29 17:00:43 +0300

    admin layout

--- nuxt-blog/layouts/admin.vue
+++ nuxt-blog/layouts/admin.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="admin-layout-wrap">
-    <el-container :style="{height: '100%'}">
-      <el-aside width="200px">
-        Aside
-      </el-aside>
-      <el-main>
-        <nuxt />
-      </el-main>
-    </el-container>
-  </div>
+    <div class="admin-layout-wrap">
+      <el-container :style="{height: '100%'}">
+        <el-aside width="250px">
+          <app-aside />
+        </el-aside>
+        <el-main>
+          <nuxt />
+        </el-main>
+      </el-container>
+    </div>
 </template>
 
 <script>
-export default {
+import AppAside from '@/components/admin/Aside'
 
+export default {
+  components: {AppAside}
 }
 </script>
 
@@ -23,5 +25,3 @@ export default {
     height: 100vh;
   }
 </style>
-
-
