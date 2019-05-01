c6a78b8a2434dda1280220d435dc1c812cbb8773
ElijahCoding <cuiyeqing960904@gmail.com>
2019-05-01 18:41:23 +0300

    user model built

--- nuxt-blog/server/controllers/auth.controller.js
+++ nuxt-blog/server/controllers/auth.controller.js
@@ -1,3 +1,7 @@
 module.exports.login = async (req, res) => {
 
 }
+
+module.exports.createUser = async (req, res) => {
+    
+}
--- nuxt-blog/server/routes/auth.routes.js
+++ nuxt-blog/server/routes/auth.routes.js
@@ -1,5 +1,9 @@
 const { Router } = require('express')
-const { login } = require('../controllers/auth.controller')
+const { login, createUser } = require('../controllers/auth.controller')
 const router = Router()
 
 router.post('/admin/login', login)
+
+router.post('/admin/create', createUser)
+
+module.exports = router
