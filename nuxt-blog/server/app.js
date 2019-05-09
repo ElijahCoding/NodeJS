5b9700aefddab36d32794329fccbe7f4f49d0d2c
ElijahCoding <cuiyeqing960904@gmail.com>
2019-05-09 17:40:13 +0300

    strategy built

--- nuxt-blog/server/app.js
+++ nuxt-blog/server/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const passport = require('passport')
+const passportStrategy = require('./middleware/passport-strategy')
 const authRoutes = require('./routes/auth.routes')
 
 const keys = require('./keys')
@@ -13,6 +14,7 @@ mongoose.connect(keys.MONGO_URI)
         .catch(error => console.error(error))
 
 app.use(passport.initialize())
+passport.use(passportStrategy)
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
