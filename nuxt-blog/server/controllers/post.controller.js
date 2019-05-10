8795581e42d4151b4f4920404618894c3575aed3
ElijahCoding <cuiyeqing960904@gmail.com>
2019-05-10 18:28:15 +0300

    post controller built

--- nuxt-blog/server/controllers/post.controller.js
+++ nuxt-blog/server/controllers/post.controller.js
@@ -16,21 +16,62 @@ module.exports.create = async (req, res) => {
 }
 
 module.exports.getAll = async (req, res) => {
-    
+    try {
+        const posts = await Post.find().sort({ date: -1 })
+        res.json(posts)
+    } catch (e) {
+        res.status(500).json(e)
+    }
 }
 
 module.exports.getById = async (req, res) => {
-
+    try {
+        await Post.findById(req.params.id)
+                  .populate('comments')
+                  .exec((error, post) => {
+                      res.json(post)
+                  })
+    } catch (e) {
+        res.status(500).json(e)
+    }
 }
 
 module.exports.update = async (req, res) => {
+    const $set = {
+        text: req.body.text
+    }
+    try {
+        const post = await Post.findOneAndUpdate({
+            _id: req.params.id
+        }, { $set }, { new: true })
 
+        res.json(post)
+    } catch (e) {
+        res.status(500).json(e)
+    }
 }
 
 module.exports.remove = async (req, res) => {
-
+    try {
+        await Post.deleteOne({ _id: req.params.id })
+        res.json({message: 'Пост удален'})
+    } catch (e) {
+        res.status(500).json(e)
+    }
 }
 
 module.exports.addView = async (req, res) => {
+    const $set = {
+        views: ++req.body.views
+    }
 
+    try {
+        await Post.findOneAndUpdate(
+            { _id: req.params.id },
+            { $set }
+        )
+        res.status(204).json()
+    } catch (e) {
+        res.status(500).json(e)
+    }
 }
