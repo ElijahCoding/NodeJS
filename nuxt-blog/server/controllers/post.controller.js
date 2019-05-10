1432e55e890f2e48bf2019be2c6ffefa778bb81f
ElijahCoding <cuiyeqing960904@gmail.com>
2019-05-10 14:25:43 +0300

    create post

--- nuxt-blog/server/controllers/post.controller.js
+++ nuxt-blog/server/controllers/post.controller.js
@@ -1,11 +1,22 @@
 const Post = require('../models/post.model')
 
 module.exports.create = async (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: `/${req.file.filename}`
+    })
 
+    try {
+        await post.save()
+        res.status(201).json(post)
+    } catch (e) {
+        res.status(500).json(e)
+    }
 }
 
 module.exports.getAll = async (req, res) => {
-
+    
 }
 
 module.exports.getById = async (req, res) => {
