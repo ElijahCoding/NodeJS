59b477bc42554c6c4f0ce2a08eeeaee3efa88056
ElijahCoding <cuiyeqing960904@gmail.com>
2019-05-11 16:04:53 +0300

    building analytics chart

--- nuxt-blog/pages/admin/index.vue
+++ nuxt-blog/pages/admin/index.vue
@@ -1,10 +1,26 @@
 <template>
-  <h1>Welcome to admin page</h1>
+  <div>
+    <h1>Аналитика по постам</h1>
+    <app-analytics-chart
+      title="Количество просмотров"
+      :labels="views.labels"
+      :data="views.data"
+    />
+
+    <app-analytics-chart
+      title="Количество комментариев"
+      :labels="comments.labels"
+      :data="comments.data"
+    />
+  </div>
 </template>
 
 <script>
+import AppAnalyticsChart from '@/components/admin/AnalyticsChart'
+
 export default {
-  layout: 'admin'
+  layout: 'admin',
+
+  middleware: ['admin-auth'],
 }
 </script>
-
