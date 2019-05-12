e1d8be15486016a3faa6c4dac3fff8b7f43e725e
ElijahCoding <cuiyeqing960904@gmail.com>
2019-05-12 14:16:46 +0300

    date filter

--- nuxt-blog/plugins/globals.js
+++ nuxt-blog/plugins/globals.js
@@ -3,5 +3,8 @@ import Element from 'element-ui'
 import VueMarkdown from 'vue-markdown'
 import locale from 'element-ui/lib/locale/lang/ru-RU'
 
+import DateFilter from '@/common/date.filter'
+
 Vue.use(Element, { locale })
 Vue.component('vue-markdown', VueMarkdown)
+Vue.filter('date', DateFilter)
--- nuxt-blog/server/app.js
+++ nuxt-blog/server/app.js
@@ -11,7 +11,7 @@ const keys = require('./keys')
 
 const app = express()
 
-mongoose.connect(keys.MONGO_URI)
+mongoose.connect(keys.MONGO_URI, { useNewUrlParser: true })
         .then(() => console.log('MongoDB connected...'))
         .catch(error => console.error(error))
 
