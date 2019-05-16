c3d2d95d69e890885eb562e21349c7114333ba20
ElijahCoding <cuiyeqing960904@gmail.com>
2019-05-16 22:34:25 +0300

    routes built

--- SurfShop/app.js
+++ SurfShop/app.js
@@ -6,8 +6,8 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const index = require('./routes/index');
-const users = require('./routes/users');
 const posts = require('./routes/posts');
+const reviews = require('./routes/reviews');
 
 const app = express();
 
@@ -24,8 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 app.use('/posts', posts);
+app.use('/posts/:id/reviews', reviews);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
