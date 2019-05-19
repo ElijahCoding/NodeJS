1b29f906c40391dc81a847231cff222f71feb9b7
ElijahCoding <cuiyeqing960904@gmail.com>
2019-05-19 17:46:59 +0300

    building layout

--- SurfShop/app.js
+++ SurfShop/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const engine = require('ejs-mate');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
@@ -27,6 +28,9 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
     console.log('Connected');
 });
+
+// use ejs-locals for all ejs templates:
+app.engine('ejs', engine);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
