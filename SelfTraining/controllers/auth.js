060037c55d7877795ad25bfeaf5970b8de8889e8
ElijahCoding <cuiyeqing960904@gmail.com>
2019-05-20 12:48:31 +0300

    register a user

--- SelfTraining/controllers/auth.js
+++ SelfTraining/controllers/auth.js
@@ -20,6 +20,19 @@ module.exports.register = async function(req, res) {
           message: 'Такой email уже занят. Попробуйте другой.'
         })
     } else {
+        // Нужно создать пользователя
+        const salt = bcrypt.genSaltSync(10)
+        const password = req.body.password
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        })
 
+        try {
+            await user.save()
+            res.status(201).json(user)
+        } catch (e) {
+            throw e
+        }
     }
 }
