67ccac496a1edec906b7c4b72014c2e884d399fe
ElijahCoding <cuiyeqing960904@gmail.com>
2019-05-23 23:45:12 +0300

    create comment mutator

--- GraphQL/src/index.js
+++ GraphQL/src/index.js
@@ -72,6 +72,7 @@ const typeDefs = `
   type Mutation {
       createUser(name: String!, email: String!, age: Int): User!
       createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+      createComment(text: String!, author: ID!, post: ID!): Comment!
   }
 
   type User {
@@ -189,6 +190,26 @@ const resolvers = {
           posts.push(post)
 
           return post
+      },
+
+      createComment (parent, args, ctx, info) {
+          const userExists = users.some(user => user.id === args.author)
+          const postExists = posts.some(post => post.id === args.post)
+
+          if (!userExists || !postExists) {
+              throw new Error('Unable to find user and post')
+          }
+
+          const comment = {
+              id: uuidv4(),
+              text: args.text,
+              author: args.author,
+              post: args.post
+          }
+
+          comments.push(comment)
+
+          return comment
       }
   },
 
