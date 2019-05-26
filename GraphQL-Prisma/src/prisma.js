e9529f50188f856d701077fe0ae9b8fe751b9d01
ElijahCoding <cuiyeqing960904@gmail.com>
2019-05-26 19:11:31 +0300

    prisma mutation

--- GraphQL-Prisma/src/prisma.js
+++ GraphQL-Prisma/src/prisma.js
@@ -5,10 +5,25 @@ const prisma = new Prisma({
   endpoint: 'http://localhost:4466'
 })
 
-prisma.query.users(null, '{ id name posts { id title } }').then((data) => {
-    console.log(JSON.stringify(data, undefined, 2))
-})
+// prisma.query.users(null, '{ id name posts { id title } }').then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2))
+// })
+//
+// prisma.query.comments(null, '{ id text author { id name } }').then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2))
+// })
 
-prisma.query.comments(null, '{ id text author { id name } }').then((data) => {
-    console.log(JSON.stringify(data, undefined, 2))
+prisma.mutation.createPost({
+    data: {
+        title: "GraphQL 101",
+        body: "",
+        published: false,
+        author: {
+            connect: {
+                id: "cjw510scn000i0933s8qsxpm4"
+            }
+        }
+    }
+}, '{ id title body published }').then(data => {
+    console.log(data);
 })
