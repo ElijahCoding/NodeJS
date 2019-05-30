b7bf4a6c14e55ef7019b73d9765cf66e8a58628e
ElijahCoding <cuiyeqing960904@gmail.com>
2019-05-30 16:22:40 +0300

    myPosts

--- GraphQL-Prisma/src/resolvers/Query.js
+++ GraphQL-Prisma/src/resolvers/Query.js
@@ -1,3 +1,5 @@
+import getUserId from '../utils/getUserId'
+
 const Query = {
     users (parent, args, { prisma }, info) {
         const opArgs = {}
@@ -13,6 +15,28 @@ const Query = {
         }
 
         return prisma.query.users(opArgs, info)
+    },
+
+    myPosts (parent, args, { prisma, request }, info) {
+        const userId = getUserId(request)
+
+        const opArgs = {
+            where: {
+                author: {
+                    id: userId
+                }
+            }
+        }
+
+        if (args.query) {
+            opArgs.where.OR = [{
+                title_contains: args.query
+            }, {
+                body_contains: args.query
+            }]
+        }
+
+        return prisma.query.posts(opArgs, info)
     },
 
     posts (parent, args, { prisma }, info) {
