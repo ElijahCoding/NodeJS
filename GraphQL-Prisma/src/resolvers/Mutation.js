56699bc16a96bc0c7b667351467f8534644ed0c7
ElijahCoding <cuiyeqing960904@gmail.com>
2019-05-30 15:05:58 +0300

    locking down user mutations

--- GraphQL-Prisma/src/resolvers/Mutation.js
+++ GraphQL-Prisma/src/resolvers/Mutation.js
@@ -53,10 +53,12 @@ const Mutation = {
         }, info)
     },
 
-    async updateUser (parent, args, { prisma }, info) {
+    async updateUser (parent, args, { prisma, request }, info) {
+        const userId = getUserId(request)
+
         return prisma.mutation.updateUser({
             where: {
-                id: args.id
+                id: userId
             },
             data: args.data
         }, info)
