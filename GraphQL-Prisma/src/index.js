b51d25f7f31d22781763c51d91a7b2014063b1d4
ElijahCoding <cuiyeqing960904@gmail.com>
2019-05-31 18:42:24 +0300

    refactoring strucutre

--- GraphQL-Prisma/src/index.js
+++ GraphQL-Prisma/src/index.js
@@ -1,22 +1,10 @@
 import { GraphQLServer } from 'graphql-yoga'
-import Query from './resolvers/Query'
-import Mutation from './resolvers/Mutation'
-import Subscription from './resolvers/Subscription'
-import User from './resolvers/User'
-import Post from './resolvers/Post'
-import Comment from './resolvers/Comment'
+import { resolvers } from './resolvers'
 import prisma from './prisma'
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers: {
-        Query,
-        Mutation,
-        Subscription,
-        User,
-        Post,
-        Comment
-    },
+    resolvers,
     context (request) {
         return {
             prisma,
--- GraphQL-Prisma/src/resolvers/User.js
+++ GraphQL-Prisma/src/resolvers/User.js
@@ -1,13 +1,16 @@
 import getUserId from '../utils/getUserId'
 
 const User = {
-    email (parent, args, { request }, info) {
-        const userId = getUserId(request, false)
+    email: {
+        fragment: 'fragment userId on User { id }',
+        resovle (parent, args, { request }, info) {
+            const userId = getUserId(request, false)
 
-        if (userId && userId === parent.id) {
-            return parent.email
-        } else {
-            return null
+            if (userId && userId === parent.id) {
+                return parent.email
+            } else {
+                return null
+            }
         }
     }
 }
