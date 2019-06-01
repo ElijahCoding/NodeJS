493e6caa85f1f46e406b13899e07f6d01ab3f675
ElijahCoding <cuiyeqing960904@gmail.com>
2019-06-01 17:47:03 +0300

    setting up jest configures

--- GraphQL-Prisma/src/index.js
+++ GraphQL-Prisma/src/index.js
@@ -1,17 +1,6 @@
-import { GraphQLServer } from 'graphql-yoga'
-import { resolvers, fragmentReplacements } from './resolvers/index'
-import prisma from './prisma'
+import '@babel/polyfill/noConflict'
+import server from './server'
 
-const server = new GraphQLServer({
-    typeDefs: './src/schema.graphql',
-    resolvers,
-    context (request) {
-        return {
-            prisma,
-            request
-        }
-    },
-    fragmentReplacements
+server.start({ port: process.env.PORT || 4000 }, () => {
+    console.log('The server is up!')
 })
-
-server.start(() => console.log('Server is running on localhost:4000'))
