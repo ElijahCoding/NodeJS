3f01b5319a39757d6b7f9392ff8b9557008b4e63
ElijahCoding <cuiyeqing960904@gmail.com>
2019-06-01 16:55:21 +0300

    some tests

--- GraphQL-Prisma/tests/user.test.js
+++ GraphQL-Prisma/tests/user.test.js
@@ -1,7 +1,32 @@
-test('This is my first test case', () => {
-    1 + 2
+import { getFirstName, isValidPassword } from '../src/utils/user.js'
+
+test('Should return first name when given full name', () => {
+    const firstName = getFirstName('Ivan Ivanoff')
+
+    expect(firstName).toBe('Ivan')
 })
 
-test('My second test ever!', () => {
 
+test('Should return first name when given first name', () => {
+    const firstName = getFirstName('Jen')
+
+    expect(firstName).toBe('Jen')
+})
+
+test('Should reject password shorter than 8 characters', () => {
+    const isValid = isValidPassword('abc123')
+
+    expect(isValid).toBe(false)
+})
+
+test('Should reject password that contains word password', () => {
+    const isValid = isValidPassword('abcPassword098')
+
+    expect(isValid).toBe(false)
+})
+
+test('Should correctly validate a valid password', () => {
+    const isValid = isValidPassword('Test123098!')
+
+    expect(isValid).toBe(true)
 })
