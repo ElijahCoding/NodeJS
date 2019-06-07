4deb58d10ae546d8c881067190a82bee3b4913a5
ElijahCoding <cuiyeqing960904@gmail.com>
2019-06-07 19:47:08 +0300

    signup signin pages built

--- vue-firebase-auth/src/views/SignIn.vue
+++ vue-firebase-auth/src/views/SignIn.vue
@@ -1,9 +1,36 @@
 <template>
-    <p>Sign in</p>
+    <div class="container">
+        <div class="columns is-centered">
+            <div class="column is-half">
+                <h1 class="title">Sign in</h1>
+
+                <form action="#">
+                    <div class="field">
+                        <label class="label">Email</label>
+                        <div class="control">
+                            <input
+                                class="input"
+                                type="text"
+                                placeholder="e.g. test@test.com"
+                            >
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <label class="label">Password</label>
+                        <div class="control">
+                            <input
+                                class="input"
+                                type="password"
+                            >
+                        </div>
+                    </div>
+
+                    <div class="control">
+                        <button class="button is-primary">Sign in</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </template>
-
-<script>
-    export default {
-
-    }
-</script>
--- vue-firebase-auth/src/views/SignUp.vue
+++ vue-firebase-auth/src/views/SignUp.vue
@@ -1,9 +1,47 @@
 <template>
-    <p>Sign up</p>
+    <div class="container">
+        <div class="columns is-centered">
+            <div class="column is-half">
+                <h1 class="title">Sign up</h1>
+
+                <form action="#">
+                    <div class="field">
+                        <label class="label">Name</label>
+                        <div class="control">
+                            <input
+                                class="input"
+                                type="text"
+                                placeholder="e.g. Elijah"
+                            >
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <label class="label">Email</label>
+                        <div class="control">
+                            <input
+                                class="input"
+                                type="text"
+                                placeholder="e.g. test@test.com"
+                            >
+                        </div>
+                    </div>
+
+                    <div class="field">
+                        <label class="label">Password</label>
+                        <div class="control">
+                            <input
+                                class="input"
+                                type="password"
+                            >
+                        </div>
+                    </div>
+
+                    <div class="control">
+                        <button class="button is-primary">Sign up</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </template>
-
-<script>
-    export default {
-
-    }
-</script>
