7f8912df03226e4e555a17713bbdc0f66c21f301
ElijahCoding <cuiyeqing960904@gmail.com>
2019-07-16 12:51:22 +0300

    render html files

--- ExpressDetails/NodeServer.js
+++ ExpressDetails/NodeServer.js
@@ -1,9 +1,16 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    res.write('<h1>Hello</h1>');
-    res.end();
+    if (req.url === '/') {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        res.write('');
+        res.end();
+    } else {
+        res.writeHead(404, { 'content-type': 'text/html' });
+        res.write('<h4>Error</h4>');
+        res.end();
+    }
 });
 
 server.listen(3000);
