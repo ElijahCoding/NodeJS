d4d70d103e0895dbba00fddda63ab9029a30d494
ElijahCoding <cuiyeqing960904@gmail.com>
2019-07-16 13:40:45 +0300

    express server details

---
+++ ExpressDetails/expressServer.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+// all is a method, and it takes 2 args;
+// 1. route
+// 2. callback to run if the route is requested
+app.all('*', (req, res) => {
+    res.send('<h1>Home page</h1>');
+});
+
+app.listen(3000);
