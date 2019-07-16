3387b7ba5e0c1105773d900b7bb04871c6af73c1
ElijahCoding <cuiyeqing960904@gmail.com>
2019-07-16 14:02:14 +0300

    static files

---
+++ ExpressDetails/expressRouting.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+
+});
+
+app.post('/', (req, res) => {
+
+});
+
+app.delete('/', (req, res) => {
+
+});
+
+app.put('/', (req, res) => {
+
+});
+app.listen(3000);
