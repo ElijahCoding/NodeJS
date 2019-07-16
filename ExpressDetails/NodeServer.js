e66740acb74711d75b13bb7afe88a29fe48cdc91
ElijahCoding <cuiyeqing960904@gmail.com>
2019-07-16 13:02:55 +0300

    loading images

--- ExpressDetails/NodeServer.js
+++ ExpressDetails/NodeServer.js
@@ -4,13 +4,20 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
         res.writeHead(200, { 'content-type': 'text/html' });
-        res.write('');
+        // res.write('');
+        const homePageHTML = fs.readFileSync('node.html');
+        res.write(homePageHTML);
+        res.end();
+    } else if (req.url === '/node.png') {
+        res.writeHead(200, { 'content-type': 'image/png' });
+        const image = fs.readFileSync('node.png');
+        res.write(image);
         res.end();
     } else {
-        res.writeHead(404, { 'content-type': 'text/html' });
-        res.write('<h4>Error</h4>');
-        res.end();
-    }
+       res.writeHead(404, { 'content-type': 'text/html' });
+       res.write('<h4>Error</h4>');
+       res.end();
+   }
 });
 
 server.listen(3000);
