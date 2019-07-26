01fe94e8821de47dbe0ebced7bf72f7e96779817
ElijahCoding <cuiyeqing960904@gmail.com>
2019-07-26 15:10:33 +0300

    set up routes

--- VueNode/server/routes/basic.js
+++ VueNode/server/routes/basic.js
@@ -6,4 +6,22 @@ const router = express.Router();
 
 router.get('/', (req, res) => res.send('Status: good'));
 
+router.get('/setup', (req, res) => {
+    User.findOne({ email: 'admin@admin.com' }, (err, doc) => {
+        if (doc != null) return res.send({});
+    });
+
+    let user = new User({
+        email: 'admin@admin.com',
+        password: bcrypt.hashSync('password', 8),
+        isAdmin: true
+    });
+
+    user.save(err => {
+        if (err) return res.send(err);
+
+        res.send({ success: true });
+    });
+});
+
 module.exports = router;
